69d41c45b2ffbd307af0d6146fef56f280e79d20
Ryan Mahoney <ryan.mahoney@betterlesson.com>
2016-11-04 20:26:51 -0400

    skeleton

---
+++ src/Model.php
@@ -0,0 +1,6 @@
+<?php
+namespace Opine/User;
+
+class Model {
+    
+}
---
+++ src/Service.php
@@ -0,0 +1,23 @@
+<?php
+namespace Opine\User;
+
+class Service {
+    public function __construct ($root)
+    {
+
+    }
+
+    public function decodeJWT ($token)
+    {
+
+    }
+
+    public function encodeJWT ()
+    {
+
+    }
+
+    public function getRoles () {
+
+    }
+}
