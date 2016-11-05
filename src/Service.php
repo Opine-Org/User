07fcabde3773c507d64f9d79bd4001a7cbcd3794
Ryan Mahoney <ryan.mahoney@betterlesson.com>
2016-11-04 20:38:55 -0400

    blind coding

--- src/Service.php
+++ src/Service.php
@@ -1,23 +1,56 @@
 <?php
 namespace Opine\User;
+use Lcobucci\JWT\Configuration;
+use Model;
 
 class Service {
-    public function __construct ($root)
-    {
+    private $root;
+    private $configService;
 
+    public function __construct ($root, $configService)
+    {
+        $this->root = $root;
+        $this->configService = $configService;
     }
 
-    public function decodeJWT ($token)
+    public function decodeJWT (string $token) : Array
     {
+        $config = new Configuration();
+        $token = $config->getParser()->parse((string) $token);
+        $token->getHeaders(); // Retrieves the token header
+        $token->getClaims();
 
+        return [
+            'id'    => $token->getClaim('id'),
+            'email' => $token->getClaim('email'),
+            'roles' => $token->getClaim('roles')
+        ];
     }
 
-    public function encodeJWT ()
+    public function encodeJWT ($id, string $email, array $roles) : string
     {
+        $config = new Configuration();
 
+        $token = $config->createBuilder()->
+            issuedBy('http://example.com')->
+            canOnlyBeUsedBy('http://example.org')->
+            identifiedBy('4f1g23a12aa', true)->
+            issuedAt(time())->canOnlyBeUsedAfter(time() + 60)->
+            expiresAt(time() + 3600)->
+            with('id', $id)->
+            with('email', $email)->
+            with('roles', $roles)->
+            getToken();
+
+        return (string)$token;
     }
 
-    public function getRoles () {
+    public function getRoles ($userId) : array
+    {
+        $roles = Model::getRoles($userId);
+    }
+
+    public function checkActivity (string $activity, array $userRoles, array $activityRoles) {
 
     }
 }
